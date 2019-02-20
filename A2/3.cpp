b7d9876ec92b3bba4f28af64cf48bcd48309b601
alizare1 <ali.zare2000123@gmail.com>
2019-02-20 22:16:08 +0330

    3 done

--- A2/3.cpp
+++ A2/3.cpp
@@ -1,102 +1,196 @@
 #include <iostream>
 #include <vector>
 
+
+#define RIGHT 0
+#define DOWN 1
+#define LEFT 2
+#define UP 3
+
+#define EMPTY '#'
+#define PLAYER 'o'
+#define BLOCK '-'
+
 using namespace std;
 
-vector<vector<char>> createBoard() {
-    vector<vector<char>> board;
-    vector<char> empty;
-    int h,w;
-    cin >> h >> w;
-    string input;
-
-    board.push_back(empty);
-    for (int i = 0; i < w + 2; i++)
-        board[0].push_back('-');
-
-    for(int i = 1; i < h + 1; i++){
-        board.push_back(empty);
-        board[i].push_back('-');
-        cin >> input;
-        for (int j = 0; j < w ; j++)
-            board[i].push_back(input[j]);
-        board[i].push_back('-');
-    }
-
-    board.push_back(empty);
-    for (int i = 0; i < w + 2; i++)
-        board[board.size() - 1].push_back('-');
-    return board;
-}
-
-bool checkWin(vector<vector<char>> board){
-    int players_count = 0;
-    for(int i = 1; i < board.size() - 1; i++){
-        for(int j = 1; j < board[i].size() - 1; j++)
-            if(board[i][j] == 'o')
-                players_count++;
-    }
-    if(players_count == 1)
-        return true;
-    return false;
-}
-
-void makeMove(vector<vector<char>>& board, int direction, int i, int j){
-    switch (direction){
-        case 0:
-            board[i][j] = '#';
-            board[i][j + 1] = '#';
-            board[i][j + 2] = 'o';
-            break;
-        case 1:
-            board[i][j] = '#';
-            board[i + 1][j] = '#';
-            board[i + 2][j] = 'o';
-        case 2:
-            board[i][j] = '#';
-            board[i][j - 1] = '#';
-            board[i][j - 2] = 'o';
-        case 3:
-            board[i][j] = '#';
-            board[i - 1][j] = '#';
-            board[i - 2][j] = 'o';
-        default:
-            break;
-    }
-}
-
-bool checkGame(vector<vector<char>> board){ // returns true when won, false when no move left
-                                            // after for loops return false
-                                            // we can use a vector for moves and copy it to temp and pass it on, if win print it all then return true. by ref?
-    if (che)
-    vector<vector<char>> temp = board;
-    for(int i = 1; i < board.size() - 1; i++) {
-        for(int j = 1; j < board[i].size() - 1; j++){
-            if(board[i][j] == 'o'){
-                if(board[i + 1][j] == 'o'){
-                    if(board[i + 2][j] == '#'){
-                        makeMove(temp, 1, i, j);
-                        if(checkWin(temp)){
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-void print(vector<vector<char>> board){
-    for(int i = 0; i < board.size(); i++){
-        for(int j = 0; j < board[i].size(); j++)
-            cout << board[i][j];
-        cout << endl;
-        }
-}
+vector<vector<char>> createBoard();
+bool checkWin(const vector<vector<char>>& board);
+void makeMove(vector<vector<char>>& board, int direction, int row_num, int col_num);
+bool checkMove(const vector<vector<char>>& board, int direction, int row_num, int col_num);
+void storeMoves(int direction, int row_num, int col_num, vector<string>& moves);
+void printBoard(vector<vector<char>> board);
+bool checkGame(vector<vector<char>> board, vector<string>& moves);
+void solve(const vector<vector<char>>& board);
+void printMoves(const vector<string>& moves);
 
 int main(){
     vector<vector<char>> board;
     board = createBoard();
-    print(board);
+	solve(board);
     return 0;
+}
+
+void solve(const vector<vector<char>>& board) {
+	vector<string> moves;
+	if (!checkGame(board, moves)) {
+		cout << "Bad map configuration!" << endl;
+		return;
+	}
+	printMoves(moves);
+}
+
+bool checkGame(vector<vector<char>> board, vector<string>& moves) {
+	if (checkWin(board)) {
+		cout << "Insane!" << endl;
+		return true;
+	}
+
+	vector<vector<char>> temp = board;
+	for (int row_num = 1; row_num < board.size() - 1; row_num++) {
+		for (int col_num = 1; col_num < board[row_num].size() - 1; col_num++) {
+			if (board[row_num][col_num] == PLAYER) {
+				for (int direction = RIGHT; direction <= UP; direction++) {
+					if (checkMove(board, direction, row_num, col_num)) {
+						makeMove(temp, direction, row_num, col_num);
+						if (checkGame(temp, moves)) {
+							storeMoves(direction, row_num, col_num, moves);
+							return true;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return false;
+
+}
+
+void printMoves(const vector<string>& moves) {
+	for (int i = moves.size() - 1; i >= 0 ; i--) 
+		cout << moves[i] << endl;
+}
+
+vector<vector<char>> createBoard() {
+	vector<vector<char>> board;
+	vector<char> empty;
+	int h, w;
+	cin >> h >> w;
+	string input;
+
+	board.push_back(empty);
+	for (int row_num = 0; row_num < w + 2; row_num++)
+		board[0].push_back(BLOCK);
+
+	for (int row_num = 1; row_num < h + 1; row_num++) {
+		board.push_back(empty);
+		board[row_num].push_back(BLOCK);
+		cin >> input;
+		for (int col_num = 0; col_num < w; col_num++)
+			board[row_num].push_back(input[col_num]);
+		board[row_num].push_back(BLOCK);
+	}
+
+	board.push_back(empty);
+	for (int row_num = 0; row_num < w + 2; row_num++)
+		board[board.size() - 1].push_back(BLOCK);
+	return board;
+}
+
+bool checkWin(const vector<vector<char>>& board) {
+	int players_count = 0;
+	for (int row_num = 1; row_num < board.size() - 1; row_num++) {
+		for (int col_num = 1; col_num < board[row_num].size() - 1; col_num++)
+			if (board[row_num][col_num] == PLAYER)
+				players_count++;
+	}
+	if (players_count == 1)
+		return true;
+	return false;
+}
+
+void makeMove(vector<vector<char>>& board, int direction, int row_num, int col_num) {
+	switch (direction) {
+	case RIGHT:
+		board[row_num][col_num] = EMPTY;
+		board[row_num][col_num + 1] = EMPTY;
+		board[row_num][col_num + 2] = PLAYER;
+		break;
+	case DOWN:
+		board[row_num + 2][col_num] = PLAYER;
+		board[row_num][col_num] = EMPTY;
+		board[row_num + 1][col_num] = EMPTY;
+		break;
+	case LEFT:
+		board[row_num][col_num] = EMPTY;
+		board[row_num][col_num - 1] = EMPTY;
+		board[row_num][col_num - 2] = PLAYER;
+		break;
+	case UP:
+		board[row_num][col_num] = EMPTY;
+		board[row_num - 1][col_num] = EMPTY;
+		board[row_num - 2][col_num] = PLAYER;
+		break;
+	default:
+		break;
+	}
+}
+
+bool checkMove(const vector<vector<char>>& board, int direction, int row_num, int col_num) {
+	switch (direction) {
+	case RIGHT:
+		if (board[row_num][col_num + 1] == PLAYER)
+			if (board[row_num][col_num + 2] == EMPTY)
+				return true;
+		break;
+	case DOWN:
+		if (board[row_num + 1][col_num] == PLAYER)
+			if (board[row_num + 2][col_num] == EMPTY)
+				return true;
+		break;
+	case LEFT:
+		if (board[row_num][col_num - 1] == PLAYER)
+			if (board[row_num][col_num - 2] == EMPTY)
+				return true;
+		break;
+	case UP:
+		if (board[row_num - 1][col_num] == PLAYER)
+			if (board[row_num - 2][col_num] == EMPTY)
+				return true;
+		break;
+	default:
+		break;
+	}
+	return false;
+}
+
+void storeMoves(int direction, int row_num, int col_num, vector<string>& moves) {
+	string move;
+	switch (direction) {
+	case RIGHT:
+		move = to_string(row_num) + " " + to_string(col_num) + " R";
+		moves.push_back(move);
+		break;
+	case DOWN:
+		move = to_string(row_num) + " " + to_string(col_num) + " D";
+		moves.push_back(move);
+		break;
+	case LEFT:
+		move = to_string(row_num) + " " + to_string(col_num) + " L";
+		moves.push_back(move);
+		break;
+	case UP:
+		move = to_string(row_num) + " " + to_string(col_num) + " U";
+		moves.push_back(move);
+	default:
+		break;
+	}
+}
+
+void printBoard(vector<vector<char>> board) {
+	for (int row_num = 0; row_num < board.size(); row_num++) {
+		for (int col_num = 0; col_num < board[row_num].size(); col_num++)
+			cout << board[row_num][col_num];
+		cout << endl;
+	}
 }
