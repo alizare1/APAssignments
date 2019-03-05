5ae72ead5e219f2299c0747e940a8bfebd242f17
alizare1 <ali.zare2000123@gmail.com>
2019-03-06 01:30:22 +0330

    Finished!

--- A3/A3.cpp
+++ A3/A3.cpp
@@ -6,7 +6,9 @@
 #include <array>
 #include <fstream>
 
-#define COURSES_CSV "courses.csv" // COMMAND LINE ARG!
+#define DAY_PIECES 28
+#define WEEK_DAYS 7
+#define NOWHERE -1
 
 using namespace std;
 
@@ -25,28 +27,141 @@ struct courseInfo {
 
 
 pair<string, string> seperateIDnAlias(string str);
-map<string, string> createCourseList();
-void addCoursesToWeek(const map<string, string>& coursesNames, const map<string, int>& days_of_week, Week& week);
+map<string, string> createCourseList(string);
+void addCoursesToWeek(const vector<string>& input, const map<string, string>& coursesNames, Week& week);
 pair<int, int> whichTimeRange(string time);
-courseInfo createCourse(string line, const map<string, int>& days_of_week);
+courseInfo createCourse(string line, const map<string, int>& daysOfWeek);
 vector<string> tokenize(string line);
 void addCourseToDay(Day& day, const pair<int, int>& timeRange, string courseName);
 void addNewTableToDay(Day& day);
 bool canCourseBeAdded(const TimeTable& table, int start, int end);
 void addCourseToTable(TimeTable& table, int start, int end, string courseName);
-void printProgram(Week);
+void printProgram(const Week&, string);
+void printTimeLine();
+int getLastCourseInTable(const TimeTable& table);
+void printCoursesBorder(const TimeTable& table);
+void printCentered(string str, int courseLength);
+void printCoursesNames(const TimeTable& table);
+vector<string> getInput();
 
-int main() {
-	map<string, int> days_of_week = {{"SAT", SAT}, {"SUN", SUN}, {"MON", MON}, {"TUE", TUE}, {"WED", WED}, {"THU", THU}, {"FRI", FRI}};
-	map<string, string> coursesNames = createCourseList();
-	Week week(7);
-	addCoursesToWeek(coursesNames, days_of_week, week);
-	printProgram(week);
+int main(int argc, char* argv[]) {
+	if (argc < 3) {
+		cout << "Not enough arguments!" << endl;
+		exit(EXIT_FAILURE);
+	}
+	map<string, string> coursesNames = createCourseList(argv[1]);
+	Week week(WEEK_DAYS);
+	vector<string> input = getInput();
+	addCoursesToWeek(input, coursesNames, week);
+	printProgram(week, argv[2]);
 	return 0;
 }
 
-printProgram(Week week){
-	;
+vector<string> getInput() {
+	vector<string> input;
+	string inputLine;
+	while (getline(cin, inputLine))
+		input.push_back(inputLine);
+	return input;
+}
+
+void printProgram(const Week& week, string progName){
+	map<int, string> daysFullName = { 
+		{SAT, "Saturday"}, {SUN, "Sunday"}, {MON, "Monday"}, {TUE, "Tuesday"}, {WED, "Wednesday"}, {THU, "Thursday"}, {FRI, "Friday"} 
+	};
+	cout << "# " << progName << endl << endl;
+	for (int day = SAT; day <= FRI; day++) {
+		cout << "## " << daysFullName[day] << endl << endl;
+		printTimeLine();
+		for (int table = 0; table < week[day].size(); table++) {
+			printCoursesBorder(week[day][table]);
+			printCoursesNames(week[day][table]);
+			printCoursesBorder(week[day][table]);
+		}
+		cout << endl;
+	}
+}
+
+void printTimeLine() {
+	for (int h = 7; h <= 9; h++) {
+		cout << '0' << h << ":00     ";
+		cout << '0' << h << ":30     ";
+	}
+	for (int h = 10; h <= 20; h++) {
+		cout << h << ":00     ";
+		if (h == 20) {
+			cout << h << ":30";
+			break;
+		}
+		cout << h << ":30     ";
+	}
+	cout << endl;
+	for (int i = 0; i < 275; i++)
+		cout << '_';
+	cout << endl;
+}
+
+int getLastCourseInTable(const TimeTable& table) {
+	for (int dayPiece = table.size() - 1; dayPiece >= 0; dayPiece--) {
+		if (table[dayPiece] != "")
+			return dayPiece;
+	}
+	return 0;
+}
+
+void printCoursesBorder(const TimeTable& table) {
+cout << setfill(' ') << setw(2) << "";
+int dayPiece = 0;
+while (dayPiece <= getLastCourseInTable(table)) {
+	int courseLength = 0;
+	string prevDayPiece;
+	if (table[dayPiece] != "") {
+		prevDayPiece = table[dayPiece];
+		while (table[dayPiece] == prevDayPiece) {
+			courseLength++;
+			dayPiece++;
+		}
+		cout << setfill('-') << setw(10 * courseLength - 1) << left << '+' << right << '+';
+	}
+	else {
+		cout << setfill(' ') << setw(10) << "";
+		dayPiece++;
+	}
+}
+cout << endl;
+}
+
+void printCoursesNames(const TimeTable& table) {
+	cout << setfill(' ') << setw(2) << "";
+	int dayPiece = 0;
+	while (dayPiece <= getLastCourseInTable(table)) {
+		int courseLength = 0;
+		string prevDayPiece;
+		if (table[dayPiece] != "") {
+			prevDayPiece = table[dayPiece];
+			while (table[dayPiece] == prevDayPiece) {
+				courseLength++;
+				dayPiece++;
+			}
+			printCentered(prevDayPiece, courseLength);
+		}
+		else {
+			cout << setfill(' ') << setw(10) << "";
+			dayPiece++;
+		}
+	}
+	cout << endl;
+
+}
+
+void printCentered(string str, int courseLength) {
+	cout << '|';
+	int space = ((courseLength * 10 - 2) - str.length()) / 2;
+	if (str.length() % 2 != 0)
+		cout << ' ';
+	cout << setw(space) << "";
+	cout << setw(str.length() + space) << left << str;
+	cout << '|';
 }
 
 pair<string, string> seperateIDnAlias(string str) {
@@ -56,10 +171,10 @@ pair<string, string> seperateIDnAlias(string str) {
 	return{ id, alias };
 }
 
-map<string, string> createCourseList() {
+map<string, string> createCourseList(string fileAddress) {
 	map<string, string> courses;
 	ifstream file;
-	file.open(COURSES_CSV);
+	file.open(fileAddress);
 	if (!file.good())
 		return{};
 	string line;
@@ -70,41 +185,59 @@ map<string, string> createCourseList() {
 	return courses;
 }
 
-courseInfo createCourse(string line, const map<string, int>& days_of_week) {
+courseInfo createCourse(string line, const map<string, int>& daysOfWeek) {
 	vector<string> tokenizedLine = tokenize(line);
 	courseInfo newCourse;
 	int numOfDays = (tokenizedLine.size() - 1) / 2;
 	newCourse.id = tokenizedLine[0].substr(0, 7);
-	newCourse.group = tokenizedLine[0].substr(9, 1);
+	if(tokenizedLine[0][8] == '0')
+		newCourse.group = tokenizedLine[0].substr(9, 1);
+	else
+		newCourse.group = tokenizedLine[0].substr(8, 2);
 	int lineIndex = 1;
 	for (int i = 0; i < numOfDays; i++) {
-		newCourse.days.push_back(days_of_week.at(tokenizedLine[lineIndex++]));
+		newCourse.days.push_back(daysOfWeek.at(tokenizedLine[lineIndex++]));
 		newCourse.times.push_back(whichTimeRange(tokenizedLine[lineIndex++]));
 	}
 	return newCourse;
 }
 
-void addCoursesToWeek(const map<string, string>& coursesNames, const map<string, int>& days_of_week, Week& week) {
-	string input;
-	while (getline(cin, input)) {
-		courseInfo newCourse = createCourse(input, days_of_week);
+void addCoursesToWeek(const vector<string>& input, const map<string, string>& coursesNames, Week& week) {
+	map<string, int> daysOfWeek = { { "SAT", SAT },{ "SUN", SUN },{ "MON", MON },{ "TUE", TUE },{ "WED", WED },{ "THU", THU },{ "FRI", FRI } };
+	for (int line = 0; line < input.size(); line++) {
+		courseInfo newCourse = createCourse(input[line], daysOfWeek);
 		string courseName = coursesNames.at(newCourse.id) + " (" + newCourse.group + ")";
 		for (int dayNO = 0; dayNO < newCourse.days.size(); dayNO++)
 			addCourseToDay(week[newCourse.days[dayNO]], newCourse.times[dayNO], courseName);
 	}
 }
 
+int findCoursePlace(const Day& day, const pair<int, int>& timeRange) {
+	int dayPiece = day.size() - 1;
+	while (dayPiece > 0) {
+		if (canCourseBeAdded(day[dayPiece], timeRange.first, timeRange.second)) {
+			if (!canCourseBeAdded(day[dayPiece - 1], timeRange.first, timeRange.second))
+				return dayPiece;
+		}
+		else
+			return NOWHERE;
+		dayPiece--;
+	}
+	if (canCourseBeAdded(day[dayPiece], timeRange.first, timeRange.second))
+		return dayPiece;
+	return NOWHERE;
+}
+
 void addCourseToDay(Day& day, const pair<int, int>& timeRange, string courseName) {
 	if (day.empty())
 		addNewTableToDay(day);
-	for (int tableNO = day.size() - 1; tableNO >= 0; tableNO--) {
-		if (canCourseBeAdded(day[tableNO], timeRange.first, timeRange.second)) {
-			addCourseToTable(day[tableNO], timeRange.first, timeRange.second, courseName);
-			return;
-		}
+	int dayPiece = findCoursePlace(day, timeRange);
+	if(dayPiece != NOWHERE)
+		addCourseToTable(day[dayPiece], timeRange.first, timeRange.second, courseName);
+	else {
+		addNewTableToDay(day);
+		addCourseToTable(day[day.size() - 1], timeRange.first, timeRange.second, courseName);
 	}
-	addNewTableToDay(day);
-	addCourseToTable(day[day.size() - 1], timeRange.first, timeRange.second, courseName);
 }
 
 void addCourseToTable(TimeTable& table, int start, int end, string courseName) {
@@ -122,7 +255,7 @@ bool canCourseBeAdded(const TimeTable& table, int start, int end) {
 }
 
 void addNewTableToDay(Day& day) {
-	TimeTable newTable(27);
+	TimeTable newTable(DAY_PIECES);
 	day.push_back(newTable);
 }
 
